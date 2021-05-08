ac65b88b02f38c093af5a9116a1d9fa0e8fdb4c8
allison <allison@intrinsik.ly>
2021-05-08 23:30:54 +0800

    Update main container

--- src/container/main/index.js
+++ src/container/main/index.js
@@ -1,24 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import TextField from '@material-ui/core/TextField';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 
-import Header from 'components/header';
+import Alert from '@material-ui/lab/Alert';
+import Divider from '@material-ui/core/Divider';
+
 import GoogleMap from 'components/map';
+import HistorySection from 'components/history-section';
+
+import { putSearchedHistories } from 'middleware/actions/response';
 
 import './style.scss';
 
+const DEFAULT_LATITUDE = 49.2827291;
+const DEFAULT_LONGITUDE = -123.1207375;
+
 const MainContainer = () => {
-  // console.log({ places: window.google.maps.places.Autocomplete });
+  const dispatch = useDispatch();
+
+  const { searchedResponses } = useSelector(state => ({ searchedResponses: state.ResponseReducer.searchedResponses }), shallowEqual);
+
+  const [selectedAddress, setAddress] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [location, setLocation] = useState({ lat: DEFAULT_LATITUDE, lng: DEFAULT_LONGITUDE });
+
+  const handleChange = address => setAddress(address);
+
+  const handleSelect = address => {
+    setAddress(address);
+    dispatch(putSearchedHistories(searchedResponses.concat(address)));
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => setLocation(latLng))
+      .catch(error => {
+        console.error(error);
+        setErrorMessage('Invalid response :(');
+      });
+  };
+
+  const handleTextClear = () => {
+    setAddress('');
+    setLocation({ lat: DEFAULT_LATITUDE, lng: DEFAULT_LONGITUDE });
+  };
+
+  const handleHistoryClear = () => dispatch(putSearchedHistories([]));
+
+  console.log({ searchedResponses });
 
   return (
     <div className="main-container">
-      <Header />
-      <div className="pa4">
-        <form className="mb3" autoComplete>
-          <TextField className="pa3" label="Location" variant="outlined" />
-        </form>
+      <div className="container--section">
+        {errorMessage && (
+          <div className="mh4 mt4">
+            <Alert severity="error">{errorMessage}</Alert>
+          </div>
+        )}
 
-        <GoogleMap />
+        <GoogleMap
+          location={location}
+          address={selectedAddress}
+          errorMessage={errorMessage}
+          onChange={handleChange}
+          onSelect={handleSelect}
+          onTextClear={handleTextClear}
+        />
+
+        <div className="section--bottom">
+          <Divider />
+          <HistorySection histories={searchedResponses} onHistoryClear={handleHistoryClear} />
+        </div>
       </div>
     </div>
   );
